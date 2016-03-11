7e36090624addf86dce91b8123c84100fb9b136d
Salvador Castro <sc@bloombees.com>
2016-03-11 09:26:22 +0100

    Connectors improvements

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once("vendor/autoload.php");
+    include_once ("ADNBP/__performance.php"); // it include __p(),_print(),_printe() funtions to trace and performance specific points
+    require_once ("ADNBP/class/ADNBP.php");
+    $adnbp = new ADNBP();
+    $adnbp -> run();
